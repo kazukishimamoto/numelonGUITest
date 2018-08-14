ec14f97f37d31c0b98dba3fd5d5c984af352e373
KazukiShimamoto <jakky0529@gmail.com>
2018-08-14 20:50:27 +0900

    keyboradButtonの配置例

--- NumelonGUItest/GUI/Keyboard.swift
+++ NumelonGUItest/GUI/Keyboard.swift
@@ -16,6 +16,10 @@ class Keyboard: AbstractKeyboard {
     required init(frame: CGRect) {
         view = KeyboardView(frame: frame)
         view.backgroundColor = .red
+        
+        // キーボードボタンの配置
+        let button = KeyboardButton(frame: CGRect(x: 0.0, y: 0.0, width: 100, height: 100))
+        self.view.addSubview(button)
     }
     
     func getView() -> UIView {
---
+++ NumelonGUItest/GUI/KeyboardButton.swift
@@ -0,0 +1,40 @@
+//
+//  KeyboardButton.swift
+//  NumelonGUItest
+//
+//  Created by shima on 2018/08/14.
+//  Copyright © 2018年 shima. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardButton: UIView {
+    //コードから生成したときに通る初期化処理
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    //InterfaceBulderで配置した場合に通る初期化処理
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    fileprivate func commonInit() {
+        //MyCustomView.xibファイルからViewを生成する。
+        //File's OwnerはMyCustomViewなのでselfとする。
+        guard let view = UINib(nibName: "KeyboardButton", bundle: nil).instantiate(withOwner: self, options: nil).first as? UIView else {
+            return
+        }
+        
+        //ここでちゃんとあわせておかないと、配置したUIButtonがタッチイベントを拾えなかったりする。
+        view.frame = self.bounds
+        
+        //伸縮するように
+        view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        //addする。viewオブジェクトの2枚重ねになる。
+        self.addSubview(view)
+    }
+}
