f5658fbfb77490823c99fcd84cd8799b4cc32add
KazukiShimamoto <jakky0529@gmail.com>
2018-08-17 16:28:59 +0900

    keyboardDisplay追加

---
+++ NumelonGUItest/GUI/KeyboardDisplay.swift
@@ -0,0 +1,21 @@
+//
+//  KeyboardDisplay.swift
+//  NumelonGUItest
+//
+//  Created by shima on 2018/08/17.
+//  Copyright © 2018年 shima. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardDisplay: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
