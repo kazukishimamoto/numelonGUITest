b56e83ef42aff5d51059ca8c6c177df1060e190f
KazukiShimamoto <jakky0529@gmail.com>
2018-08-18 11:39:36 +0900

    キーボードディスプレイのxib&swiftファイル編集

--- NumelonGUItest/GUI/KeyboardDisplay.swift
+++ NumelonGUItest/GUI/KeyboardDisplay.swift
@@ -9,4 +9,28 @@
 import UIKit
 
 class KeyboardDisplay: UIView {
+    var currentMsg: NSString = ""
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+//        self.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func display(msg: String) {
+        print("Display: \(msg)")
+        currentMsg = NSString(string: msg)
+        setNeedsDisplay()
+    }
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        self.backgroundColor = .white
+        currentMsg.draw(at: CGPoint(x: self.frame.minX + 100.0, y: self.frame.minY + 100.0), withAttributes: nil)
+    }
 }
