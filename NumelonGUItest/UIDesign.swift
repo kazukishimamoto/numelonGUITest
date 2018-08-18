23eabcf8dde26d77bd0e5ab4570e599b9bfb5622
mat <mat@macbook-pro.local>
2018-08-18 14:08:42 +0900

    いらないの削除

--- NumelonGUItest/UIDesign.swift
+++ NumelonGUItest/UIDesign.swift
@@ -18,61 +18,6 @@ struct UIDesign {
     }
 }
 
-enum Message: String {
-    case Clear = "Clear"
-    case Failed = "GameOver"
-    
-    static func getHintMsg(inputtedAns: [String], eat: Int, bite: Int) -> String {
-        var convertedMsg = ""
-        for str in inputtedAns {
-            convertedMsg += str
-        }
-        return "\(convertedMsg) \(eat)EAT\(bite)BITE"
-    }
-    static func getInfoByHintMsg(msg: String) -> (inputtedAns: [String], eat: Int, bite: Int)? {
-        // change format into "ans,eat,bite"
-        var str = msg
-        if let range = str.range(of: " ") {
-            str.replaceSubrange(range, with: ",")
-        }else {
-            return nil
-        }
-        if let range = str.range(of: "EAT") {
-            str.replaceSubrange(range, with: ",")
-        }else {
-            return nil
-        }
-        if let range = str.range(of: "BITE") {
-            str.replaceSubrange(range, with: ",")
-        }else {
-            return nil
-        }
-        
-        let convertedStr = str.components(separatedBy: ",")
-        var inputtedAns = [String]()
-        for s in convertedStr[0] {
-            inputtedAns.append(String(s))
-        }
-        guard let eat = Int(convertedStr[1]) else {
-            return nil
-        }
-        guard let bite = Int(convertedStr[2]) else {
-            return nil
-        }
-        return (inputtedAns, eat, bite)
-    }
-    
-    static func getClearMsg(answer: [String]) -> String {
-        return Message.Clear.rawValue
-    }
-    static func getFailedMsg(answer: [String]) -> String {
-        var appendMsg = ""
-        for s in answer {
-            appendMsg += s
-        }
-        return Message.Failed.rawValue + " Answer: " + appendMsg
-    }
-}
 
 
 
